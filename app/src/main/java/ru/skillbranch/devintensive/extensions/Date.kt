e1aeadc19d4959e191aa4e6055ff5254b9edabfc
Nikita519 <53587551+Nikita519@users.noreply.github.com>
2020-04-19 03:01:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,39 @@
+package ru.skillbranch.devintensive.extensions
+
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value: Int, units: TimeUtils = TimeUtils.SECONDS): Date {
+    var time = this.time
+
+    time +=when (units) {
+        TimeUtils.SECONDS -> value * SECOND
+        TimeUtils.MINUTE -> value * MINUTE
+        TimeUtils.HOUR -> value * HOUR
+        TimeUtils.DAY -> value * DAY
+    }
+    this.time = time
+    return this
+}
+
+fun Date.humanizeDiff(date:Date = Date()): String {
+    TODO()
+}
+
+enum class TimeUtils{
+    SECONDS,
+    MINUTE,
+    HOUR,
+    DAY
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,195 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+
+        val parts: List<String>? = fullName?.split(" ")
+
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+
+        if (fullName == "" || fullName == null || fullName == " ") {
+            return null to null
+        }
+        if (lastName == null) {
+            return firstName to null
+        }
+        return firstName to lastName
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+
+        var fSymbol = firstName?.get(0)
+        var lSymbol = lastName?.get(0)
+
+        if(firstName == null && lastName == null || firstName == " " || lastName == " " || firstName == "" || lastName == ""){
+            return null
+        }
+        var f = when (fSymbol) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else -> fSymbol
+        }
+
+        if (lastName == null){
+            return "$f"
+        }
+        var l = when (lSymbol) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else -> lSymbol
+        }
+
+        return "$f$l"
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+
+        val parts: List<String>? = payload?.split(divider)
+
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+
+        var f = firstName?.replace(Regex("[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]")) {
+            when (it.value) {
+                "а" -> "a"
+                "б" -> "b"
+                "в" -> "v"
+                "г" -> "g"
+                "д" -> "d"
+                "е" -> "e"
+                "ё" -> "e"
+                "ж" -> "zh"
+                "з" -> "z"
+                "и" -> "i"
+                "й" -> "i"
+                "к" -> "k"
+                "л" -> "l"
+                "м" -> "m"
+                "н" -> "n"
+                "о" -> "o"
+                "п" -> "p"
+                "р" -> "r"
+                "с" -> "s"
+                "т" -> "t"
+                "у" -> "u"
+                "ф" -> "f"
+                "х" -> "h"
+                "ц" -> "c"
+                "ч" -> "ch"
+                "ш" -> "sh"
+                "щ" -> "sh'"
+                "ъ" -> ""
+                "ы" -> "i"
+                "ь" -> ""
+                "э" -> "e"
+                "ю" -> "yu"
+                "я" -> "ya"
+                else -> it.value
+            }
+        }
+
+        var l = lastName?.replace(Regex("[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]")) {
+            when (it.value) {
+                "а" -> "a"
+                "б" -> "b"
+                "в" -> "v"
+                "г" -> "g"
+                "д" -> "d"
+                "е" -> "e"
+                "ё" -> "e"
+                "ж" -> "zh"
+                "з" -> "z"
+                "и" -> "i"
+                "й" -> "i"
+                "к" -> "k"
+                "л" -> "l"
+                "м" -> "m"
+                "н" -> "n"
+                "о" -> "o"
+                "п" -> "p"
+                "р" -> "r"
+                "с" -> "s"
+                "т" -> "t"
+                "у" -> "u"
+                "ф" -> "f"
+                "х" -> "h"
+                "ц" -> "c"
+                "ч" -> "ch"
+                "ш" -> "sh"
+                "щ" -> "sh'"
+                "ъ" -> ""
+                "ы" -> "i"
+                "ь" -> ""
+                "э" -> "e"
+                "ю" -> "yu"
+                "я" -> "ya"
+                else -> it.value
+            }
+        }
+
+        return f + divider + l
+    }
+}
